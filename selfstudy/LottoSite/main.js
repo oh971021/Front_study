0e52fcad07546f9b2312bcd5aec9695bb9117007
Junseok <oh971021@gmail.com>
2021-10-08 00:34:55 +0900

    1008 Java Script Study : Lotto

--- selfstudy/LottoSite/main.js
+++ selfstudy/LottoSite/main.js
@@ -1,10 +1,3 @@
-var lotto = [];
-for (var i=0 i<6 i++) {
-    var num = Math.floor(Math.random() + 44) + 1;
-    for (var j in lotto) {
-        if (num == lotto[j]) {
-            num = Math.floor(Math.random() + 44) + 1;
-        }
-    }
+function button1_click() {
+    console.log("버튼을 누르셨습니다.");
 }
-document.write('행운의 번호 : ' + lotto);
