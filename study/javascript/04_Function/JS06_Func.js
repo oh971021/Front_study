a75c0ccddd8daa601937f0d6e9bdc685949e8782
Junseok <oh971021@gmail.com>
2021-10-08 12:01:26 +0900

    1008 Java Script Study : Submit

--- study/javascript/04_Function/JS06_Func.js
+++ study/javascript/04_Function/JS06_Func.js
@@ -24,3 +24,28 @@ function callGuGuDan(n) {
         console.log(n + 'x' + i + '=' + n*i);
     }
 }
+
+function printGugudan2() {
+    let myInput = document.myForm.i1;
+    let myValue = myInput.value;
+
+    alert(`${myValue}단을 출력합니다`);
+
+    for (let i = 1; i < 10; i++) {
+        console.log(myValue + ' x ' + i + ' = ' + myValue*i)
+    }
+
+    // 디테일 잡기
+
+    // input 박스 값 초기화 시켜주기
+    myInput.value = "";
+
+    // input 박스에 포커스를 잡아주기
+    myInput.focus();
+
+    // return 사용 이유 ?
+    // Form Tag를 사용했기 때문이다.
+    // 원래 submit 하면 어딘가로 제출이 된다.
+    // 그래서 어딘가로 사라지지않고 머무르기 위해서
+    return false;
+}
